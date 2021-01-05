321b60178b916650893f8cfa28309ef47fb036be
Trevor Mearns <trevor@trevormearns.com>
2021-01-05 13:03:24 +0900

    js refactor finished

--- ExpenseApp.js
+++ ExpenseApp.js
@@ -6,39 +6,47 @@ const dateInput = document.getElementById('date');
 const amountInput = document.getElementById('amount');
 const locationInput = document.getElementById('purchase');
 const descriptionInput = document.getElementById('description');
+
+const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
 // listeners
+
+window.addEventListener('load', () => {
+  expenses.forEach((expense) => {
+    addRow(expense);
+  });
+});
 
 const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const newExpense = {
+    id: Math.random(),
     time: dateInput.value,
     total: amountInput.value,
     location: locationInput.value,
     item: descriptionInput.value
   };
   addRow(newExpense);
+  saveExpense(newExpense);
+  updateAmount(newExpense);
 });
-table.addEventListener('click', deleteRow);
-table.addEventListener('change', updateAmount);
+
+//table.addEventListener('change', updateAmount);
 //window.addEventListener('load', addRow);
 
 // functions
-const tableCell2 = document.createElement('td');
 
 function addRow(expense) {
+  const id = expense.id;
   const tableRow = document.createElement('tr');
+  tableRow.setAttribute('id', id);
   table.appendChild(tableRow);
   const deleteButton = document.createElement('BUTTON');
   const deleteButtonText = document.createTextNode('X');
   // creates new table cells top to bottom = right to left in table
   const tableCell1 = document.createElement('td');
   tableRow.appendChild(tableCell1);
-
-  tableCell2.setAttribute('class', 'amount');
-  tableCell2.setAttribute('type', 'number');
-  tableCell2.setAttribute('value', '0');
-  tableCell2.setAttribute('min', '0');
+  const tableCell2 = document.createElement('td');
   tableRow.appendChild(tableCell2);
   const tableCell3 = document.createElement('td');
   tableRow.appendChild(tableCell3);
@@ -47,6 +55,7 @@ function addRow(expense) {
   tableRow.appendChild(deleteButton);
   deleteButton.appendChild(deleteButtonText);
   deleteButton.setAttribute('class', 'delete-button');
+  deleteButton.addEventListener('click', deleteRow);
 
   tableCell1.textContent = expense.time;
   tableCell2.textContent = expense.total;
@@ -58,17 +67,40 @@ function addRow(expense) {
   updateAmount();
 }
 
-function deleteRow(event) {
-  if (event.target.tagName !== 'BUTTON') return; // targets tagName that are NOT buttons and will return them.
-  const row = event.target.closest('tr'); // targets closest table row
-  row.parentElement.removeChild(row); //action to remove specific row
-  updateAmount();
+function saveExpense(expense) {
+  expenses.push(expense);
+  pushToLocalStorage(expense);
 }
 
-function updateAmount() {
-  let total = 0;
-  const totalPlus = tableCell2.innerText;
-  total += Number(totalPlus);
-  totalElement.textContent = total.toFixed(2);
+function pushToLocalStorage(expense) {
+  localStorage.setItem('expenses', JSON.stringify(expenses));
+}
+function deleteRow(e) {
+  console.log('I fired.');
+  e.preventDefault();
+  let rowId = e.target.parentNode.id;
+  console.log('row id: ', e.target.parentNode);
+  for (let i = 0; i < expenses.length; i++) {
+    if (expenses[i].id === Number(rowId)) {
+      expenses.splice(i, 1);
+      targetRow = document.getElementById(rowId);
+      targetRow.parentNode.removeChild(targetRow);
+      localStorage.setItem('expenses', JSON.stringify(expenses));
+      updateAmount(expenses);
+      if (expenses.length === 0) totalElement.textContent = '$' + 0;
+    }
+  }
+}
+
+function updateAmount(value) {
+  let sum = 0;
+  expenses.forEach(({ total }) => {
+    sum += parseFloat(total);
+    totalElement.textContent = sum;
+  });
+  // let total = 0;
+  // const totalPlus = tableCell2.innerText;
+  // total += Number(totalPlus);
+  // totalElement.textContent = total.toFixed(2);
 }
 //display total of expenses .tofixed adds two decimal points.
